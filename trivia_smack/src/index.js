30910194f7e81bd454c1d52498f6c64ea8ce887c
jpagnan0 <a.jpagnano@gmail.com>
2019-01-23 18:13:05 -0500

    set category layout

--- trivia_smack/src/index.js
+++ trivia_smack/src/index.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const app = new App();
-  // app.attachEventListeners();
-  // debugger
-Promise.all(app.adapter.fetchCategories()).then(data => data.flat()).then(data => app.createCategory(data))
-Promise.all(app.adapter.fetchClues()).then(data => data.flat()).then(data => app.createClues(data))
+	const app = new App();
+	// app.attachEventListeners();
+	// debugger
+	Promise.all(app.adapter.fetchCategories()).then(data => data.flat()).then(data => app.createCategory(data))
+	Promise.all(app.adapter.fetchClues()).then(data => data.flat()).then(data => app.createClues(data))
 
-// app.adapter.fetchClues().then(app.createClues)
+	// app.adapter.fetchClues().then(app.createClues)
 })
