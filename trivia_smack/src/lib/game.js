d1200ebdd3bd72e03b5869ae85527f6e0232a1ca
jpagnan0 <a.jpagnano@gmail.com>
2019-01-25 12:39:51 -0500

    add static code for use with user info

--- trivia_smack/src/lib/game.js
+++ trivia_smack/src/lib/game.js
@@ -11,3 +11,27 @@ class Game {
   }
 }
 Game.all = []
+
+let gameStats = document.createElement('div');
+gameStats.setAttribute("class", "container");
+let statString = `<div class="level">
+       <div class="level-left">
+	       <p id="user-name" class="level-item is-size-5"> User: <span class="has-text-weight-light is-italic">user name here</span></p><br>
+	       <p id="score" class="level-item is-size-5"> Score: <span class="has-text-weight-light is-italic">score here</span></p>
+       </div>
+      </div>`;
+gameStats.innerHTML = statString;
+let parent = document.querySelector('#title');
+parent.insertBefore(gameStats, parent.childNodes[1])
+
+//pass in user_id get back user element
+function setId(uid) {
+    let user = document.getElementById('user-name')
+    user.setAttribute('data-id', uid)
+    return user
+}
+//pass in element get back added
+function setDataId(el, uid) {
+    el.setAttribute('data-id', uid)
+    return el
+}
